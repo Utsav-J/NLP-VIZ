95cb5651c9d5e0bb076198b6817830a0c2817504
UtsavJ <utsavjaiswal2004@gmail.com>
2025-10-21 22:41:58 +0530

    uvicorn endpoint

--- main.py
+++ main.py
@@ -98,5 +98,5 @@ def main():
     print(json.dumps(result.model_dump(), indent=2))
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=9000)
